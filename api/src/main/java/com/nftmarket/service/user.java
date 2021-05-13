a968206158f147d5fe67f5049e34a5b5d7ff82c0
kula <kula@live.com>
2021-05-13 22:28:43 +0800

    fix: add user service;

---
+++ api/src/main/java/com/nftmarket/service/user.java
@@ -0,0 +1,25 @@
+package com.nftmarket.service;
+
+import java.net.URI;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.POST;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import io.smallrye.mutiny.Uni;
+
+@Path("/user")
+public class user {
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Uni<Response> createUser(){
+        return Uni.createFrom().item("hello")
+        .map(item -> URI.create(item))
+        .map(uri ->{
+            return Response.created(uri).build();
+        });
+
+    }
+    
+}
